a012c0870b12da209f7041c38a674fdd6377a237
Hugo Vega <hugo17avid@gmail.com>
2024-11-02 23:08:43 -0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from 'dotenv';
 import { MongoClient, ObjectId } from 'mongodb';
+import db from './db/conn.mjs';
 
 dotenv.config();
 
@@ -11,8 +12,17 @@ import grades from "./routes/grades.mjs";
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the API.");
+app.get("/", async (req, res) => {
+  let collection = db.collection("grades");
+  let newDocument = {
+    learner_id: 1,
+    class_id: 301,
+  }
+  let result = await collection.insertOne(newDocument).catch( e => {
+    return e.errInfo.details.schemaRulesNotSatisfied;
+  });
+  res.send(result).status(204);
+  // res.send("Welcome to the API.");
 });
 
 app.use("/grades", grades);
@@ -22,6 +32,31 @@ app.use((err, _req, res, next) => {
   console.error(err);
   res.status(500).send("Seems like we messed up somewhere...");
 });
+
+  // Update validation rules
+  await db.command({
+    collMod: 'grades',
+    validator: {
+      $jsonSchema: {
+        bsonType: 'object',
+        required: ['class_id', 'learner_id'],
+        properties: {
+          class_id: {
+            bsonType: 'int',
+            minimum: 0,
+            maximum: 300,
+            description: 'must be an integer in [0, 300] and is required'
+          },
+          learner_id: {
+            bsonType: 'int',
+            minimum: 0,
+            description: 'must be an integer greater than or equal to 0 and is required'
+          }
+        }
+      }
+    },
+    validationAction: 'warn'
+  });
 
 const uri = process.env.ATLAS_URI;
 const client = new MongoClient(uri);
